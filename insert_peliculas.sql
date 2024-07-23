74c840b041f34a66a870b26f5d4259bdb5599ac3
Onai <emilianomarchenko@gmail.com>
2024-07-23 18:02:51 -0300

    Añadir script SQL para insertar datos en la tabla Pelicula

---
+++ insert_peliculas.sql
@@ -0,0 +1,4 @@
+INSERT INTO Pelicula (codigo, titulo, url, imagen, generos) VALUES
+('001', '2001: Odisea del Espacio', 'http://www.example.com/2001', 'http://www.example.com/2001.jpg', 'Ciencia ficción,Drama'),
+('002', 'El Padrino', 'http://www.example.com/padrino', 'http://www.example.com/padrino.jpg', 'Crimen,Drama'),
+('003', 'La Guerra de las Galaxias', 'http://www.example.com/starwars', 'http://www.example.com/starwars.jpg', 'Acción,Aventura,Fantasia');
